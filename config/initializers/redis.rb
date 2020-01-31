04bee73359a2ce4597e612a47d85575244d1d5dc
TatsuroMoriyama <t4traw@gmail.com>
2020-01-31 11:35:32 +0900

    Fix deprecated method
    changed engine_name from parent_name

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,13 +1,13 @@
 case Rails.env
 when "procuction"
-  namespace = [Rails.application.class.parent_name, Rails.env].join(':')
+  namespace = [Rails.application.class.engine_name, Rails.env].join(':')
   Redis.current = Redis::Namespace.new(namespace,
     redis: Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT']), password: ENV['REDIS_PASSWORD'])
 when "development"
-  namespace = [Rails.application.class.parent_name, Rails.env].join(':')
+  namespace = [Rails.application.class.engine_name, Rails.env].join(':')
   Redis.current = Redis::Namespace.new(namespace,
     redis: Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT']))
 when "test"
-  namespace = [Rails.application.class.parent_name, Rails.env].join(':')
+  namespace = [Rails.application.class.engine_name, Rails.env].join(':')
   Redis.current = Redis::Namespace.new(namespace, redis: MockRedis.new)
 end
