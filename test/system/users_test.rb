3f3d27401de48d01911abced46752403032edeca
TatsuroMoriyama <t4traw@gmail.com>
2020-01-31 11:48:18 +0900

    通知テストの追加

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class WikisTest < ApplicationSystemTestCase
   setup do
     # setup_user
-    @test_user = {
+    @new_user = {
       name: Faker::Games::Zelda.character,
       email: Faker::Internet.email,
       password: "password"
@@ -19,10 +19,19 @@ class WikisTest < ApplicationSystemTestCase
     visit login_path
     click_link "ユーザー登録はこちら"
     assert_text "ユーザー登録"
-    fill_in "user[email]", with: @test_user[:email]
-    fill_in "user[password]", with: @test_user[:password]
+    fill_in "user[email]", with: @new_user[:email]
+    fill_in "user[password]", with: @new_user[:password]
     find('#user_agreement_label').click
     click_button "登録"
     assert_text "確認メールを送信しました"
+  end
+
+  test "receive first notification if user resistered" do
+    user = create_user(@new_user)
+    login_user(@new_user)
+    visit user_notices_path(user.id)
+    page.save_screenshot "tmp/screenshots/#{Time.now.strftime('%Y%m%d%H%M%S')}.png"
+    assert_text "通知一覧"
+    assert_text "ようこそ"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+# require 'minitest/reporters'
+# Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -9,13 +11,31 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  def setup_user
+  def create_user(u)
+    visit new_user_path
+    fill_in "user[email]", with: u[:email]
+    fill_in "user[password]", with: u[:password]
+    find('#user_agreement_label').click
+    find('#user_resister_submit').click
+    User.last.activate!
+    User.last
+  end
+
+  def login_user(u)
+    visit login_url
+    fill_in "email", with: u[:email]
+    fill_in "password", with: u[:password]
+    find('#user_login_submit').click
+  end
+
+  def fixture_user
+    # fixtureを使うと登録時のnoticeなどがないので注意
     user = users(:mario)
     user.update!(password: 'password')
     visit login_url
-    fill_in "login_email", with: user[:email]
-    fill_in "login_password", with: 'password'
-    find('#user_login').click
-    sleep 0.1
+    fill_in "email", with: user[:email]
+    fill_in "password", with: 'password'
+    find('#user_login_submit').click
+    user
   end
 end
