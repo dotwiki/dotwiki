bd53bcb72e9e08df1a75670fad5c2285ef2a8f4e
Tatsuro Moriyama <t4traw@gmail.com>
2020-02-04 22:51:03 +0900

    Fix typo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 case Rails.env
-when "procuction"
+when "production"
   namespace = [Rails.application.class.engine_name, Rails.env].join(':')
   Redis.current = Redis::Namespace.new(namespace,
     redis: Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT']), password: ENV['REDIS_PASSWORD'])
