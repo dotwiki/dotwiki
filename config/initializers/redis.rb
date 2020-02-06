22b8cdbf6a8fce3062736f951c2e18506ab4f47b
TatsuroMoriyama <t4traw@gmail.com>
2020-02-06 15:01:08 +0900

    Fix syntax error

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,12 +1,10 @@
 case Rails.env
 when "production"
   namespace = [Rails.application.class.engine_name, Rails.env].join(':')
-  Redis.current = Redis::Namespace.new(namespace,
-    redis: Redis.new(url: ENV['REDIS_URL'])
+  Redis.current = Redis::Namespace.new(namespace, redis: Redis.new(url: ENV['REDIS_URL']))
 when "development"
   namespace = [Rails.application.class.engine_name, Rails.env].join(':')
-  Redis.current = Redis::Namespace.new(namespace,
-    redis: Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT']))
+  Redis.current = Redis::Namespace.new(namespace, redis: Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT']))
 when "test"
   namespace = [Rails.application.class.engine_name, Rails.env].join(':')
   Redis.current = Redis::Namespace.new(namespace, redis: MockRedis.new)
