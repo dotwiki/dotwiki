66b7498aadfd9de6d99f6fc0115b2d643f90ccb6
TatsuroMoriyama <t4traw@gmail.com>
2020-02-06 14:56:55 +0900

    Fix redis env
    Heroku redisはなんのタイミングか分からないけど、ポートとかパスワードが勝手に変わるので、自動で設定されるREDIS_URLに変更

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,7 +2,7 @@ case Rails.env
 when "production"
   namespace = [Rails.application.class.engine_name, Rails.env].join(':')
   Redis.current = Redis::Namespace.new(namespace,
-    redis: Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT']), password: ENV['REDIS_PASSWORD'])
+    redis: Redis.new(url: ENV['REDIS_URL'])
 when "development"
   namespace = [Rails.application.class.engine_name, Rails.env].join(':')
   Redis.current = Redis::Namespace.new(namespace,
