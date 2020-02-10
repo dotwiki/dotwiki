254bda0e0369e0679fb8a20b2786d79ab5d7cfcd
TatsuroMoriyama <t4traw@gmail.com>
2020-02-10 14:49:43 +0900

    Formatted

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get '/thanks', to: "home#thanks"
+  root "home#index"
+  get "/thanks", to: "home#thanks"
 
   resources :wikis, except: [:destroy], param: :wiki_id do
     member do
-      get '/about', to: 'wikis#about'
-      post '/upload', to: 'wikis#upload'
+      get "/about", to: "wikis#about"
+      post "/upload", to: "wikis#upload"
       resource :nav, only: [:edit, :update], module: "wikis"
       resources :maintainers, except: %i[show new edit], module: "wikis"
+      resources :attachments, except: %i[show], module: "wikis"
       resources :pages, except: [:destroy], module: "wikis", as: :wiki_pages do
         resources :requests do
-          get '/adjust', to: 'requests#adjust'
-          post '/merge', to: 'requests#merge'
-          post '/reject', to: 'requests#reject'
+          get "/adjust", to: "requests#adjust"
+          post "/merge", to: "requests#merge"
+          post "/reject", to: "requests#reject"
         end
       end
     end
@@ -32,8 +33,8 @@ Rails.application.routes.draw do
   get "oauth/callback", to: "oauths#callback"
   get "oauth/callback/:provider", to: "oauths#callback"
   get "oauth/login_from/:provider", to: "oauths#oauth", as: :auth_at_provider
-  get 'login', to: 'user_sessions#new', as: :login
-  post 'logout', to: 'user_sessions#destroy', as: :logout
+  get "login", to: "user_sessions#new", as: :login
+  post "logout", to: "user_sessions#destroy", as: :logout
   resources :user_sessions, only: [:new, :create, :destroy]
   resources :password_resets, only: [:new, :create, :edit, :update]
 end
