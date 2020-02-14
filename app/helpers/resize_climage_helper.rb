90c1b953251fb6fcca6de3242a326960c41d6ade
TatsuroMoriyama <t4traw@gmail.com>
2020-02-14 15:49:25 +0900

    cloudinaryのベース名を環境変数に変更

--- app/helpers/resize_climage_helper.rb
+++ app/helpers/resize_climage_helper.rb
@@ -1,8 +1,7 @@
 module ResizeClimageHelper
   def resize_climage(path, opt)
     if Rails.env.production?
-      # https://res.cloudinary.com/dotwiki/image/upload/v1581132842/thyswdnpefll9yageewh.png
-      base_url = 'https://res.cloudinary.com/dotwiki/image/upload/'
+      base_url = "https://res.cloudinary.com/#{ENV['CLOUDINARY_CLOUD_NAME']}/image/upload/"
       path.slice!(base_url)
       base_url + opt + '/' + path
     else
