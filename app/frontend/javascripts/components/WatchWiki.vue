c893acdac40eab51a16b7230be14f21ccdc1b56a
TatsuroMoriyama <t4traw@gmail.com>
2020-02-18 09:27:48 +0900

    WatchWikiボタンでエラーが出る問題を修正

--- app/frontend/javascripts/components/WatchWiki.vue
+++ app/frontend/javascripts/components/WatchWiki.vue
@@ -1,38 +1,26 @@
 <template>
-  <div>
-    <a v-if="watchingWiki" class @click="toggleWatch()">
-      <svg
-        style="width: 2rem; height: 2rem;"
-        xmlns="http://www.w3.org/2000/svg"
-        width="512"
-        height="512"
-        viewBox="0 0 512 512"
-      >
+  <div class="watch_wiki">
+    <a v-if="watchingWiki" class @click="toggleWatch()" data-turbolinks="false">
+      <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
         <path
           d="M474 74.2c-50.7-50.7-132.9-50.7-183.6 0-17.6 17.5-29 38.9-34.4 61.4-5.4-22.5-16.8-43.8-34.4-61.4-50.7-50.7-132.9-50.7-183.6 0-50.7 50.7-50.7 132.9 0 183.5l218 218 218-218c50.7-50.6 50.7-132.8 0-183.5z"
           fill="#fc7972"
         />
       </svg>
     </a>
-    <a v-else class @click="toggleWatch()">
-      <svg
-        style="width: 2rem; height: 2rem;"
-        xmlns="http://www.w3.org/2000/svg"
-        width="512"
-        height="512"
-        viewBox="0 0 512 512"
-      >
+    <a v-else class @click="toggleWatch()" data-turbolinks="false">
+      <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
         <path
           d="M474 74.2c-50.7-50.7-132.9-50.7-183.6 0-17.6 17.5-29 38.9-34.4 61.4-5.4-22.5-16.8-43.8-34.4-61.4-50.7-50.7-132.9-50.7-183.6 0-50.7 50.7-50.7 132.9 0 183.5l218 218 218-218c50.7-50.6 50.7-132.8 0-183.5z"
           fill="#d9d9d9"
         />
       </svg>
     </a>
+    <p class="show-xl">このWikiをお気に入りに登録する</p>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
 import { csrfToken } from '@rails/ujs'
 import axios from 'axios'
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
