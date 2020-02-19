c86be89fa896e082c960047dc5a8af3d8e19c6ee
TatsuroMoriyama <t4traw@gmail.com>
2020-02-19 11:33:41 +0900

    indexページから最新のwikiを削除

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :require_login
 
   def index
-    @recent_created_wikis = Wiki.order(id: :desc).limit(10)
     if current_user
       @waching_wikis = current_user&.watches.map { |v| v.wiki }
       @news = Notification.where(wiki_id: current_user.watches.pluck(:wiki_id))
